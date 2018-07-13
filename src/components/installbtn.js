9f2c1c53b7e17d9778f51ad4f6b70eac0a54efcd
Uri <oriol.castroarnau@gmail.com>
2018-07-13 19:21:29 +0200

    Install button

--- src/components/installbtn.js
+++ src/components/installbtn.js
@@ -15,21 +15,21 @@ class InstallBtn extends Component {
     }))
   }
 
-  showInstallPrompt(deferredPrompt) {
-    // hide our user interface that shows our A2HS button
-    this.toogleInstallIcon()
-    // Show the prompt
-    deferredPrompt.prompt
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then(choiceResult => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt')
-      } else {
-        console.log('USer dismissed the A2HS prompt')
-      }
-      deferredPrompt = null
-    })
-  }
+  // showInstallPrompt(deferredPrompt) {
+  //   // hide our user interface that shows our A2HS button
+  //   this.toogleInstallIcon()
+  //   // Show the prompt
+  //   deferredPrompt.prompt()
+  //   // Wait for the user to respond to the prompt
+  //   deferredPrompt.userChoice.then(choiceResult => {
+  //     if (choiceResult.outcome === 'accepted') {
+  //       console.log('User accepted the A2HS prompt')
+  //     } else {
+  //       console.log('USer dismissed the A2HS prompt')
+  //     }
+  //     deferredPrompt = null
+  //   })
+  // }
 
   componentDidMount() {
     let deferredPrompt
@@ -39,11 +39,22 @@ class InstallBtn extends Component {
       console.log(e.platforms)
       e.preventDefault()
       // Stash the event so it can be triggered later.
-      console.log(e)
       deferredPrompt = e
-      console.log(deferredPrompt)
-      this.event = e
       this.toogleInstallIcon()
+    })
+    document.getElementById(btnInstall).addEventListener('click', e => {
+      // hide our user interface that shows our A2HS button
+      this.toogleInstallIcon() // Show the prompt
+      deferredPrompt.prompt()
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt')
+        } else {
+          console.log('User dismissed the A2HS prompt')
+        }
+        deferredPrompt = null
+      })
     })
   }
 
@@ -51,14 +62,9 @@ class InstallBtn extends Component {
     return (
       <div>
         {this.state.installIcon && (
-          <div className="installbtn--wrapper">
+          <div className="installbtn--wrapper" id="btnInstall">
             <span className="installbtn--text">Instalar</span>
-            <Button
-              icon
-              secondary
-              id="btnInstall"
-              onClick={this.showInstallPrompt}
-            >
+            <Button icon secondary>
               cloud_download
             </Button>
           </div>
