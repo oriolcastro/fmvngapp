3b2a33a6a7f40b72d1eed69ff6e05360d27a8ed6
Uri <oriol.castroarnau@gmail.com>
2018-07-13 18:30:59 +0200

    Small change

--- src/components/installbtn.js
+++ src/components/installbtn.js
@@ -38,6 +38,7 @@ class InstallBtn extends Component {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault()
       // Stash the event so it can be triggered later.
+      console.log(e)
       deferredPrompt = e
       console.log(deferredPrompt)
       this.event = e
