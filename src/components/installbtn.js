c7ca5e2da354349b57131e28ca5f79708cdd441f
Uri <oriol.castroarnau@gmail.com>
2018-07-13 17:22:16 +0200

    A2HS error

--- src/components/installbtn.js
+++ src/components/installbtn.js
@@ -6,12 +6,29 @@ class InstallBtn extends Component {
     super(props)
     this.state = { installIcon: false }
     this.toogleInstallIcon = this.toogleInstallIcon.bind(this)
+    this.showInstallPrompt = this.showInstallPrompt.bind(this)
   }
 
   toogleInstallIcon() {
     this.setState(prevState => ({
       installIcon: !prevState.installIcon,
     }))
+  }
+
+  showInstallPrompt() {
+    // hide our user interface that shows our A2HS button
+    this.toogleInstallIcon()
+    // Show the prompt
+    deferredPrompt.prompt()
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt')
+      } else {
+        console.log('USer dismissed the A2HS prompt')
+      }
+      deferredPrompt = null
+    })
   }
 
   componentDidMount() {
@@ -25,28 +42,28 @@ class InstallBtn extends Component {
       this.toogleInstallIcon()
     })
 
-    btnInstall.addEventListener('click', e => {
-      // hide our user interface that shows our A2HS button
-      this.toogleInstallIcon()
-      // Show the prompt
-      deferredPrompt.prompt()
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt')
-        } else {
-          console.log('USer dismissed the A2HS prompt')
-        }
-        deferredPrompt = null
-      })
-    })
+    // btnInstall.addEventListener('click', e => {
+    //   // hide our user interface that shows our A2HS button
+    //   this.toogleInstallIcon()
+    //   // Show the prompt
+    //   deferredPrompt.prompt()
+    //   // Wait for the user to respond to the prompt
+    //   deferredPrompt.userChoice.then(choiceResult => {
+    //     if (choiceResult.outcome === 'accepted') {
+    //       console.log('User accepted the A2HS prompt')
+    //     } else {
+    //       console.log('USer dismissed the A2HS prompt')
+    //     }
+    //     deferredPrompt = null
+    //   })
+    // })
   }
 
   render() {
     return (
       <div>
         {this.state.installIcon && (
-          <FontIcon id="btnInstall">cloud_download</FontIcon>
+          <FontIcon onClick={this.showInstallPrompt}>cloud_download</FontIcon>
         )}
       </div>
     )
