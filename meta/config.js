0aaa2cfc46de4475b14e1698ef4bfb252e8a110f
Uri <oriol.castroarnau@gmail.com>
2018-07-13 12:57:59 +0200

    Update config

--- meta/config.js
+++ meta/config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: "Programa FMVng '18",
   siteShortName: 'FMVNG18', // Site title.
   siteTitleAlt: 'Programa FM Vilanova 2018', // Alternative site title for SEO.
-  siteLogo: '/icons/icon.png', // Logo used for SEO and manifest.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'http://okstudio-fmvngapp.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription:
