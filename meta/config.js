e845cc3551dfd2d85820e8bec7575572f6197302
Uri <oriol.castroarnau@gmail.com>
2018-07-19 14:44:36 +0200

    Change primary color

--- meta/config.js
+++ meta/config.js
@@ -16,7 +16,7 @@ module.exports = {
     'Vilanova',
     'FMVng',
   ], // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#FFC107',
+  themeColor: '#FFA000',
   backgroundColor: '#fff',
   googleAnalytics: 'UA-121776060-2',
 }
