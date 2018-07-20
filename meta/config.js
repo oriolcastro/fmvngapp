c7fa89cb88966a17c03f26cc1b206054a7fdc0c3
Uri <oriol.castroarnau@gmail.com>
2018-07-20 17:08:00 +0200

    Update domain

--- meta/config.js
+++ meta/config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteShortName: 'FMVNG18', // Site title.
   siteTitleAlt: 'Programa FM Vilanova 2018', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'http://okstudio-fmvngapp.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://fmvng.app', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
   siteDescription:
     "Aplicació web progressiva amb tot el programa d'activitats de la Festa Major de Vilanova i la Geltrú", // Website description used for RSS feeds/meta description tag.
